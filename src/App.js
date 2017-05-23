025e4a4367343c7897d6be593687d130962ea2d3
Adi <drumsapkota@gmail.com>
2017-05-23 15:04:49 +0530

    learning stateless and stateful components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+/*
+class App extends React.Component{
+  render(){
+    return <h1>Hello JSX</h1>
+  }
+}
+*/
 
-const App = () => <h1>Hello React!! </h1>
+const App = () => <h1> Hello stateless </h1>
 
 export default App
