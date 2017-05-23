37b5aade2adc1c088e7d9dadf86bfbfa472aeac3
Adi <drumsapkota@gmail.com>
2017-05-23 16:19:52 +0530

    Learning how state works with constructor usage  and custom function bindings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state text'
+    }
+  }
+  update(e){
+    this.setState({txt:e.target.value})
+  }
+
   render(){
     let txt = this.props.txt
     return (<div>      
               <h1>Hello JSX</h1>
-              <h2>{txt}</h2>
+              <input type="text" onChange={this.update.bind(this)}/>
+              <h2>{this.state.txt}</h2>
             </div>)
   }
 }
 
 
-App.propTypes = {
-  txt:React.PropTypes.string,
-  cat:React.PropTypes.number.isRequired
-}
 
-
-App.defaultProps = {
-  txt:"this is the default prop"
-}
 //const App = () => <h1> Hello stateless </h1>
 
 export default App
