5de7fb7d70d9fc36c7b85d62a66c9eb32fa1e669
Adi <drumsapkota@gmail.com>
2017-05-23 16:06:03 +0530

    Understanding props and prop types, but apparently the way that I did it has been deprecated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-/*
+
 class App extends React.Component{
   render(){
-    return <h1>Hello JSX</h1>
+    let txt = this.props.txt
+    return (<div>      
+              <h1>Hello JSX</h1>
+              <h2>{txt}</h2>
+            </div>)
   }
 }
-*/
 
-const App = () => <h1> Hello stateless </h1>
+
+App.propTypes = {
+  txt:React.PropTypes.string,
+  cat:React.PropTypes.number.isRequired
+}
+
+
+App.defaultProps = {
+  txt:"this is the default prop"
+}
+//const App = () => <h1> Hello stateless </h1>
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App  cat={6}/>, document.getElementById('root'));
 registerServiceWorker();
