55e0ab88b61e39a17445570ca3d67b9fb4f3ebe5
Adi <drumsapkota@gmail.com>
2017-05-24 16:27:49 +0530

    learning life cycle methods

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 class App extends React.Component{
-  constructor() {
+  constructor(){
     super();
-    this.state = {a:''}
+    this.state = {val: 0}
+    this.update = this.update.bind(this)
   }
   update(){
-    this.setState({a:ReactDOM.findDOMNode(this.a).value,
-                   b:this.refs.b.value})
+    this.setState({val:this.state.val + 1})
   }
   render(){
+    console.log("render");
     return (<div> 
-             <input
-             ref={component => this.a = component}
-             type="text" 
-             onChange={this.update.bind(this)}
-            />{this.state.a}
-            <hr/>
-            <input
-            ref="b"
-            type="text"
-            onChange={this.update.bind(this)}
-            />{this.state.b}     
+            <button onClick={this.update}>{this.state.val}</button>
             </div>)
-  }
-}
-
-class Input extends React.Component{
-  render(){
-    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
   }
 }
 
