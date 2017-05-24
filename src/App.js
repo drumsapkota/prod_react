bedd45d5df942033fa826460f56f492d0bdc9437
Adi <drumsapkota@gmail.com>
2017-05-24 15:32:14 +0530

    Using reactDom and find dom node

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 class App extends React.Component{
   constructor() {
     super();
     this.state = {a:''}
   }
   update(){
-    this.setState({a:this.refs.a.value,
+    this.setState({a:ReactDOM.findDOMNode(this.a).value,
                    b:this.refs.b.value})
   }
   render(){
     return (<div> 
              <input
-             ref="a"
+             ref={component => this.a = component}
              type="text" 
              onChange={this.update.bind(this)}
             />{this.state.a}
@@ -26,7 +26,11 @@ class App extends React.Component{
   }
 }
 
-
+class Input extends React.Component{
+  render(){
+    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
+  }
+}
 
 
 
