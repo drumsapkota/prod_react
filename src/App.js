e2bfc94a52b8c6cd7a22a47ab468332ca26129d9
Adi <drumsapkota@gmail.com>
2017-05-24 16:43:52 +0530

    Lifecycle methods implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 class App extends React.Component{
   constructor(){
     super();
@@ -9,15 +9,41 @@ class App extends React.Component{
   update(){
     this.setState({val:this.state.val + 1})
   }
+  componentDidMount() {
+    console.log("Component did mount");
+  } 
   render(){
     console.log("render");
     return (<div> 
             <button onClick={this.update}>{this.state.val}</button>
             </div>)
   }
+  componentWillMount() {
+    console.log("Component will mount");
+  }
+  componentWillUnmount() {
+    console.log("Component will unmount");
+  }
+  componentDidUpdate() {
+    console.log("Component was updated")
+  }
+}
+
+class Wrapper extends React.Component{
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return(<div>
+              <button onClick={this.mount.bind(this)}>Mount</button>
+              <button onClick={this.unmount.bind(this)}>Un Mount</button>
+              <div id="a"></div>
+           </div>)
+  }
 }
 
 
-
-
-export default App
+export default Wrapper
